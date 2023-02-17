491471a03fd64c9a03b9f7c23634509c5ccbbb5b
Franco Minati <francominati2@gmail.com>
2023-02-17 16:23:34 -0300

    agregue un toque de logica para hacer el puntaje y agregue una clase pronostico para ese calcular ese puntaje

--- src/main/java/entities/Partido.java
+++ src/main/java/entities/Partido.java
@@ -14,21 +14,27 @@ public class Partido {
 
     private Equipo equipo1;
     private Equipo equipo2;
+    private Equipo equipoGanador;
     private int golesEquipo1;
     private int golesEquipo2;
 
-public void ganadorDelpartido(){
+public Equipo ganadorDelpartido(){
+    equipo1.addGolestoEquipo(this.golesEquipo1);
+    equipo2.addGolestoEquipo(this.golesEquipo2);
     if(this.golesEquipo1 > this.golesEquipo2){
         System.out.println(equipo1.getNombre()+" "+ Resultado.GANADOR);
         System.out.println(equipo2.getNombre()+" "+ Resultado.PERDEDOR);
+        return equipo1;
     } else if (this.golesEquipo1 == this.golesEquipo2) {
         System.out.println(equipo1.getNombre()+ " AND "+ equipo2.getNombre()+ " "+ Resultado.EMPATE);
+        return new Equipo();
+
     }else{
         System.out.println(equipo2.getNombre()+" "+ Resultado.GANADOR);
         System.out.println(equipo1.getNombre()+" "+ Resultado.PERDEDOR);
+        return equipo2;
     }
-    equipo1.addGolestoEquipo(this.golesEquipo1);
-    equipo2.addGolestoEquipo(this.golesEquipo2);
+    
 }
 
 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,10 @@
 import entities.Equipo;
 import entities.Partido;
+import entities.Pronostico;
 import entities.Rondas;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class main {
@@ -12,17 +14,34 @@ public class main {
         Equipo e3 = new Equipo("Polonia", 0);
         Equipo e4 = new Equipo("Peru", 0);
 
-        Partido p1 = new Partido(e1, e2, 3, 1);
-        Partido p2 = new Partido(e3, e4, 1, 3);
-        Partido pFinal = new Partido(e2, e3, 1, 5);
+        Partido p1 = new Partido(e1, e2,null, 3, 1);
+        Partido p2 = new Partido(e3, e4,null, 1, 3);
+        Partido pFinal = new Partido(e2, e3,null, 1, 5);
+
+        int puntaje=0;
+        Pronostico pr1=new Pronostico();
+        List<Equipo> pronosticos=new ArrayList<Equipo>();
+        pronosticos.add(e1);
+        pronosticos.add(e3);
+        pronosticos.add(e3);
+        pr1.setEquipos(pronosticos);
 
         System.out.println("Partido 1");
+        if(pr1.getEquipos().get(0).equals(p1.ganadorDelpartido())){
+            puntaje++;
+        }
         p1.ganadorDelpartido();
         System.out.println("Partido 2");
+        if(pr1.getEquipos().get(1).equals(p2.ganadorDelpartido())){
+            puntaje++;
+        }
         p2.ganadorDelpartido();
         System.out.println("Partido final");
+        if(pr1.getEquipos().get(2).equals(pFinal.ganadorDelpartido())){
+            puntaje++;
+        }
         pFinal.ganadorDelpartido();
-
+        
         Rondas ronda1 = new Rondas("Numero 1", new ArrayList<>());
 
         ronda1.añadirPartidos(p1);
@@ -30,6 +49,7 @@ public class main {
         ronda1.añadirPartidos(pFinal);
 
         ronda1.golesEquipos();
+        System.out.println("Tu puntaje es: "+puntaje);
 
     }
 }
