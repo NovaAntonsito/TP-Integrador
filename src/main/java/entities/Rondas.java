4d0804fa88527436a1391ce3a6ee81b15cab106f
Marcos Anton <marcos.anton@alumnos.frm.utn.edu.ar>
2023-02-27 20:57:59 -0300

    Saque el lombok y conecte a una base de datos

--- src/main/java/entities/Rondas.java
+++ src/main/java/entities/Rondas.java
@@ -1,27 +1,30 @@
 package entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Rondas  {
     private int nroRonda;
     private Equipo equipoGanador;
 
-    /*public void armadorDeGrupos(ArrayList<Equipo> listaEquipos1,ArrayList<Equipo> listaEquipos2){
-        for(int i=0;i<Math.pow(2,this.nroRonda);i++){
-            if(i<Math.pow(2,this.nroRonda)/2){
-                listaEquipos1.add(new Equipo());
-            }else{
-                listaEquipos2.add(new Equipo());
-            }
-        }
-    }*/
+    public Rondas(int nroRonda, Equipo equipoGanador) {
+        this.nroRonda = nroRonda;
+        this.equipoGanador = equipoGanador;
+    }
+
+    public Rondas() {
+    }
+
+    public int getNroRonda() {
+        return nroRonda;
+    }
+
+    public void setNroRonda(int nroRonda) {
+        this.nroRonda = nroRonda;
+    }
+
+    public Equipo getEquipoGanador() {
+        return equipoGanador;
+    }
+
+    public void setEquipoGanador(Equipo equipoGanador) {
+        this.equipoGanador = equipoGanador;
+    }
 }
