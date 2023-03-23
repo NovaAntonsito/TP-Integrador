8edf0fb1e23dca65d8ec56dd5f990e6899712793
Franco Minati <francominati2@gmail.com>
2023-03-23 11:01:39 -0300

    videos

--- src/main/java/entities/Grupos.java
+++ src/main/java/entities/Grupos.java
@@ -7,10 +7,24 @@ import java.util.List;
 public class Grupos {
 
     private List<Equipo> listaEquipo;
+    private char nombreGrupo;
+
+
 
     public Grupos() {
     }
 
+    public Grupos(char nombreGrupo) {
+        this.nombreGrupo = nombreGrupo;
+    }
+
+    public char getNombreGrupo() {
+        return nombreGrupo;
+    }
+
+    public void setNombreGrupo(char nombreGrupo) {
+        this.nombreGrupo = nombreGrupo;
+    }
     public Grupos(List<Equipo> listaEquipo) {
         this.listaEquipo = listaEquipo;
     }
